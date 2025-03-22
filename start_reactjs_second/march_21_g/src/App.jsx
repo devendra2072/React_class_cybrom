9ab2cf3e8f2a6e3941c526e30f1e0dacdd82c362
devendra2027 <devendrakurve094@gmail.com>
2025-03-22 13:52:39 +0530

    react

--- start_reactjs_second/march_21_g/src/App.jsx
+++ start_reactjs_second/march_21_g/src/App.jsx
@@ -1,144 +1,44 @@
-// import { useReducer } from "react";
+// import { useState,useMemo } from "react";
 
 // const App = () => {
-//   const reducerFun = (state, action) => {
-//     switch (action) {
-//       case "Increment":
-//         return state + 1;
-//       case "Decrement":
-//         return state - 1;
-//       default:
-//         return state;
-//     }
-//   };  // Missing semicolon and closing brace
-
-//   const [count, mydispatch] = useReducer(reducerFun, 0);
-
+//   const [count, setCount] = useState(0);
+//   const [mysub, setMySub] = useState(100);
+//   const multiplication=useMemo(()=>{
+//     console.log("Multiplication");
+//     return count*2;
+//   },[count]);
 //   return (
 //     <>
-//       <h1>Welcome to my useReducer Hook App</h1>
-//       <button onClick={() => mydispatch("Increment")}>Increment</button>
-//       <h2>{count}</h2>
-//       <button onClick={() => mydispatch("Decrement")}>Decrement</button>
+//       <h1>Welcome to useMemo Hook App</h1>
+//       <h2>Multiplication: {multiplication}</h2>
+//       <button onClick={()=>{setCount(count+1)}}>Addition</button>
+//       <h1>Addition :{count}</h1>
+//       <button onClick={()=>{setMySub(mysub-1)}}>Subtraction</button>
+//       <h1>Subtraction :{mysub}</h1>
 //     </>
 //   );
 // };
-
-// export default App;
-
-
-
-
-
-
-
-
-
-// import { useReducer } from "react";
-
-// const App = () => {
-//   const colorChange = (state, action) => {
-//     switch (action) {
-//       case "Green":
-//         return state = "green";
-//       case "Red":
-//         return state = "red";
-//       case "Blue":
-//         return state = "blue";
-//       case "Aqua":
-//         return state = "aqua";
-//       default:
-//         return state;
-//     }
-//   };  // Missing semicolon and closing brace
-
-//   const [myColor, dispatch] = useReducer(colorChange, "black");
-
-//   return (
-//     <>
-//       <h1>Welcome to my useReducer color Change App</h1>
-//       <button onClick={()=>{dispatch("Green")}}>Green</button>
-//       <button onClick={()=>{dispatch("Red")}}>Red</button>
-//       <button onClick={()=>{dispatch("Blue")}}>Blue</button>
-//       <button onClick={()=>{dispatch("Aqua")}}>Aqua</button>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-
-// import { useReducer } from "react";
-
-// const App = () => {
-//   const colorChange = (state, action) => {
-//     switch (action) {
-//       case "Green":
-//         return "green";
-//       case "Red":
-//         return "red";
-//       case "Blue":
-//         return "blue";
-//       case "Aqua":
-//         return "aqua";
-//       default:
-//         return state;
-//     }
-//   };
-
-//   const [myColor, dispatch] = useReducer(colorChange, "black");
-
-//   return (
-//     <>
-//       <h1>Welcome to my useReducer color Change App</h1>
-//       <button onClick={() => {dispatch("Green")}}>Green</button>
-//       <button onClick={() => {dispatch("Red")}}>Red</button>
-//       <button onClick={() => {dispatch("Blue")}}>Blue</button>
-//       <button onClick={() => {dispatch("Aqua")}}>Aqua</button>
-//       {/* Added style to demonstrate the color change */}
-//       <div style={{ backgroundColor: myColor, width: '100px', height: '100px' }}></div>
-//     </>
-//   );
-// };
-
 // export default App
 
 
 
-
-
-import { useReducer } from "react";
+import { useState,useMemo } from "react";
 
 const App = () => {
-  const colorChange = (state, action) => {
-    switch (action) {
-      case "Green":
-        return "green";
-      case "Red":
-        return "red";
-      case "Blue":
-        return "blue";
-      case "Aqua":
-        return "aqua";
-      default:
-        return state;
-    }
-  };
-
-  const [myColor, dispatch] = useReducer(colorChange, "black");
-
+  const [num, setNum] = useState(0);
+  const [subject, setSubject] = useState("");
+  const myVal = useMemo(()=>{
+    for(var i=0; i<1000000000; i++){}
+    return num*2;
+  }, [num] );
+  
   return (
     <>
-      <h1>Welcome to my useReducer color Change App</h1>
-      <button onClick={() => {dispatch("Green")}}>Green</button>
-      <button onClick={() => {dispatch("Red")}}>Red</button>
-      <button onClick={() => {dispatch("Blue")}}>Blue</button>
-      <button onClick={() => {dispatch("Aqua")}}>Aqua</button>
-      {/* Added style to demonstrate the color change */}
-      <div style={{ color: myColor, width: '100px', height: '100px',border:'5px solid', marginTop:"50px" }}>Welcome to cybrom</div>
+      select number : <input type="number" value={num} onChange={(e)=>{setNum(parseInt(e.target.value))}} />
+      Enter subject : <input type="text" value={subject} onChange={(e)=>{setSubject(e.target.value)}} />
+      <br />
+      my Counting : {myVal}
     </>
   );
 };
-
 export default App
